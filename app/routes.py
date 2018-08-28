ff49cb3fb5d33f87662df8f1fce2702a66955a2a
Patrik Setterberg <setterberg.patrik@gmail.com>
2018-08-28 16:58:00 +0200

    first commit

---
+++ app/routes.py
@@ -0,0 +1,91 @@
+from flask import render_template, flash, redirect, request, url_for
+from flask_login import current_user, login_user, logout_user, login_required
+from app import app, db
+from app.models import User
+from app.forms import LoginForm, RegistrationForm
+from datetime import datetime
+from werkzeug.urls import url_parse
+
+
+# Home
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html', title='Home')
+
+
+# Survive game
+@app.route('/survive')
+def survive():
+    return render_template('survive.html', title='Survive: Type or die!')
+
+
+# Login
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+
+        return redirect(next_page)
+
+    return render_template('login.html', title='Sign in', form=form)
+
+
+# Logout
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+
+# Register
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        flash("You're already registered and logged in.")
+        return redirect(url_for('index'))
+
+    form = RegistrationForm()
+
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data) # NIFTY
+        db.session.add(user)
+        db.session.commit()
+
+        flash('Success! You are now a registered user.')
+        return redirect(url_for('login'))
+    
+    return render_template('register.html', title='Register', form=form)
+
+
+# User page
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    
+    return render_template('user.html', title='Profile: ' + user.username,
+                           user=user)
+
+
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
