aea00ef5b41a2c1442fcf4e49226d43964645d28
Patrik Setterberg <setterberg.patrik@gmail.com>
2018-09-01 18:42:29 +0200

    Email - reset passwords and fixed edit_user

--- app/forms.py
+++ app/forms.py
@@ -1,3 +1,4 @@
+from flask import render_template, flash, redirect, url_for
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired, Email, EqualTo 
@@ -12,9 +13,12 @@ class LoginForm(FlaskForm):
 
 
 class RegistrationForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
+    username = StringField('Username', validators=[DataRequired(),
+                           Length(min=3, max=20)])
+    email = StringField('Email', validators=[DataRequired(), Email(),
+                        Length(min=5, max=50)])
+    password = PasswordField('Password', validators=[DataRequired(),
+                             Length(min=6, max=50)])
     password2 = PasswordField('Repeat Password', validators=[
                               DataRequired(), EqualTo('password')])
     submit = SubmitField('Register')
@@ -31,8 +35,9 @@ class RegistrationForm(FlaskForm):
     
 
 class EditUsernameForm(FlaskForm):
-    username = StringField('Change Username')
-    submit = SubmitField('Submit')
+    username = StringField('Change Username', validators=[DataRequired(),
+                           Length(min=3, max=20)])
+    submit_username = SubmitField('Submit')
 
     def __init__(self, original_username, *args, **kwargs):
         super(EditUsernameForm, self).__init__(*args, **kwargs)
@@ -42,13 +47,13 @@ class EditUsernameForm(FlaskForm):
         if username.data != self.original_username:
             user = User.query.filter_by(username=self.username.data).first()
             if user is not None:
-                flash('Please use a different username.')
-                return redirect(url_for('edit_user'))
+                raise ValidationError('Please use a different username.')
 
 
 class EditEmailForm(FlaskForm):
-    email = StringField('Change E-Mail Address', validators=[DataRequired(), Email()])
-    submit = SubmitField('Submit')
+    email = StringField('Change E-Mail Address', validators=[DataRequired(),
+                        Email(), Length(min=5, max=50)])
+    submit_email = SubmitField('Submit')
 
     def __init__(self, original_email, *args, **kwargs):
         super(EditEmailForm, self).__init__(*args, **kwargs)
@@ -58,11 +63,26 @@ class EditEmailForm(FlaskForm):
         if email.data != self.original_email:
             mail = User.query.filter_by(email=self.email.data).first()
             if mail is not None:
-                flash('Please choose a different e-mail address.')
-                return redirect(url_for('edit_user'))
+                raise ValidationError('Please choose a different e-mail address.')
+                
 
 
 class EditPasswordForm(FlaskForm):
-    password = PasswordField('Change Password')
-    password2 = PasswordField('Repeat Password')
-    submit = SubmitField('Submit')
+    password = PasswordField('Change Password', validators=[DataRequired(),
+                             Length(min=6, max=50)])
+    password2 = PasswordField('Repeat Password', validators=[
+                              DataRequired(), EqualTo('password')])
+    submit_password = SubmitField('Submit')
+
+
+class ResetPasswordRequestForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    submit = SubmitField('Request Password Reset')
+
+
+class ResetPasswordForm(FlaskForm):
+    password = PasswordField('Password', validators=[DataRequired(),
+                             Length(min=6, max=50)])
+    password2 = PasswordField('Repeat Password', validators=[
+                              DataRequired(), EqualTo('password')])
+    submit = SubmitField('Request Password Reset')
--- app/routes.py
+++ app/routes.py
@@ -2,11 +2,13 @@ from flask import render_template, flash, redirect, request, url_for
 from flask_login import current_user, login_user, logout_user, login_required
 from app import app, db
 from app.models import User
-from app.forms import LoginForm, RegistrationForm
+from app.forms import LoginForm, RegistrationForm, ResetPasswordRequestForm
 from app.forms import EditEmailForm, EditPasswordForm, EditUsernameForm
+from app.forms import ResetPasswordForm
 from datetime import datetime
 from werkzeug.urls import url_parse
 from werkzeug.security import generate_password_hash
+from app.email import send_password_reset_email
 
 
 # Home
@@ -94,44 +96,72 @@ def edit_user():
     mail_form = EditEmailForm(current_user.email)
     pass_form = EditPasswordForm()
 
-    # Change username
-    if user_form.validate_on_submit():
-        if len(user_form.username.data) < 3 or len(user_form.username.data) > 20:
-            flash('Username must be between 3 and 20 characters.')
-            return redirect(url_for('edit_user'))
-        current_user.username = user_form.username.data
-        db.session.commit()
-        flash('Username updated successfully!')
-        return redirect(url_for('edit_user'))
-    # Change email
-    elif mail_form.validate_on_submit():
-        # NEED EMAIL FORMAT VALIDATION
-        # A LITTLE BIT OF REGEXP MAYBE. 
-        # IF NOT alpha @ alphas . alphas
-        current_user.email = mail_form.email.data
-        db.session.commit()
-        flash('Email updated successfully!')
-        return redirect(url_for('edit_user'))
-    # Change password
-    elif pass_form.validate_on_submit():
-        if len(pass_form.password.data) < 5 or len(pass_form.password.data) > 40:
-            flash('password must be between 5 and 40 characters.')
-            return redirect(url_for('edit_user'))
-        elif pass_form.password.data != pass_form.password2.data:
-            flash('Passwords do not match. Please try again.')
-            return redirect(url_for('edit_user'))
-        else:
+    if request.method == 'POST':
+    
+        item = ''
+
+        # Change username
+        if user_form.submit_username.data and user_form.validate():
+            current_user.username = user_form.username.data
+            db.session.commit()
+            item = 'Username'
+    
+        # Change email
+        elif mail_form.submit_email.data and mail_form.validate():
+            current_user.email = mail_form.email.data
+            db.session.commit()
+            item = 'Email'
+    
+        # Change password
+        elif pass_form.submit_password.data and pass_form.validate():
             current_user.password_hash = generate_password_hash(pass_form.password.data)
             db.session.commit()
-            flash('Password changed successfully!')
-            return redirect(url_for('edit_user'))
-    elif request.method == 'GET':
-        user_form.username.data = current_user.username
-        mail_form.email.data = current_user.email
-    
+            item = 'Password'
+
+        flash('{} updated successfully!'.format(item))
+        return redirect(url_for('edit_user'))
+            
     return render_template('edit_user.html', title='Edit user details',
                            user_form=user_form, mail_form=mail_form,
                            pass_form=pass_form)
+
+
+@app.route('/reset_password_request', methods=['GET', 'POST'])
+def reset_password_request():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    
+    form = ResetPasswordRequestForm()
+
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user:
+            send_password_reset_email(user)
+        flash('Check your email for instructions to reset your password.')
+        return redirect(url_for('login'))
+        
+    return render_template('reset_password_request.html', 
+                           title='Reset Password', form=form)
+
+
+@app.route('/reset_password/<token>', methods=['GET', 'POST'])
+def reset_password(token):
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    
+    user = User.verify_reset_password_token(token)
+    if not user:
+        return redirect(url_for('index'))
+
+    form = ResetPasswordForm()
+
+    if form.validate_on_submit():
+        user.set_password(form.password.data)
+        db.session.commit()
+        flash('Your password has been reset.')
+        return redirect(url_for('login'))
+
+    return render_template('reset_password.html', form=form)
 
 @app.before_request
 def before_request():
